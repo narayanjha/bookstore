9b13d5ca1c35cc7efb9f8e815443b3e0ef4bf5e7
Narayan Jha <jha_narayanji@yahoo.com>
2026-05-09 08:44:55 +0530

    Client setup
    Client initial setup

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        }),
+    provideRouter(routes)
+  ]
+};
